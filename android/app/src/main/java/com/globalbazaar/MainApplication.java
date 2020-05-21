a0b69adc27ba04b333fe88d4db61a837b129315b
Kobid <kobidkunda193@gmail.com>
2020-05-21 15:44:06 +0530

    initial

--- android/app/src/main/java/com/globalbazaar/MainApplication.java
+++ android/app/src/main/java/com/globalbazaar/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -33,7 +34,14 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+
+         @Override
+                protected String getJSBundleFile() {
+                    return CodePush.getJSBundleFile();
+                }
       };
+
+
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -46,6 +54,8 @@ public class MainApplication extends Application implements ReactApplication {
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
+
+
 
   /**
    * Loads Flipper in React Native templates. Call this in the onCreate method with something like
